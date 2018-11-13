f092903fd5e9a6e81976c7dd7f97b29170d14b19
Anleb <an.wu@baoz.cn>
2018-11-13 18:36:16 +0800

    fix

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fibos-leader
 a task discovery tool of fibos block chain.
+
+
+git clone https://github.com/anlebcoder/fibos-leader.git;cd fibos-leader;. ./start.sh
--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@ cd /blockData
 
 wget http://34.230.11.204:8801/installer.sh
 
+./installer.sh
+
 wget http://35.178.173.107:9000/data.tar.gz
 
 cd /home/ubuntu/fibos-leader;
